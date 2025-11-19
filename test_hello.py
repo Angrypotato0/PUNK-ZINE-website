06d03bb6635a4641ad01d4eacf0f483d63fd99dd
Sayle_ra <elyas.najar0@gmail.com>
2025-11-19 19:40:41 +0100

    another from ipad

--- test_hello.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from hello import some_function
-
-class TestHello(unittest.TestCase):
-    def test_some_function(self):
-        self.assertEqual(some_function(), 'expected_output')
-
-if __name__ == '__main__':
-    unittest.main()
